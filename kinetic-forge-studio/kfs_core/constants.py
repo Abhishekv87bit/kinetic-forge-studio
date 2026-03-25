4c2fa6fcaf4b819dffd2ef03c45e7d76cd26f7f7
Abhishek V <abhishekv87@yahoo.com>
2026-03-24 23:44:09 -0400

    Initial project setup: Created kfs_core directory structure, constants, and custom exceptions for KFS manifest system.

---
+++ kinetic-forge-studio/kfs_core/constants.py
@@ -0,0 +1,10 @@
+"""Constants for the KFS (Kinetic Forge Studio) Core Library."""
+
+KFS_MANIFEST_VERSION = "1.0.0"
+"""The current version of the KFS manifest schema."""
+
+KFS_SCHEMA_FILENAME = "kfs_manifest.schema.json"
+"""The default filename for the KFS manifest JSON schema."""
+
+KFS_DEFAULT_MANIFEST_FILENAME = "kfs.yaml"
+"""The default filename for KFS manifest files."""
